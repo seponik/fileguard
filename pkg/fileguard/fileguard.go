07aa19340bd0a65333a42e816b796bd572365255
seponik <sepoxmain@gmail.com>
2025-07-22 11:25:27 +0000

    Add comments to clarify function usage in fileguard.go

--- pkg/fileguard/fileguard.go
+++ pkg/fileguard/fileguard.go
@@ -2,10 +2,16 @@ package fileguard
 
 import "github.com/seponik/fileguard/internal/crypto"
 
+// EncryptFile encrypts the give file using AES-GCM with the given key.
+// Returns error if encryption fails.
+// Creates a file with the same name and path, but with a .fg extension.
 func EncryptFile(filePath, key string) error {
 	return crypto.EncryptFile(filePath, key)
 }
 
+// DecryptFile decrypts the give file using AES-GCM with the given key.
+// Returns error if decryption fails.
+// Creates a file with the same name and path, but without .fg extension.
 func DecryptFile(filePath, key string) error {
 	return crypto.DecryptFile(filePath, key)
 }
